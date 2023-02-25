6bfee0dd6dcba49f0ac5665f1213c70c21a31dc2
juliano <julianopaulo.santos@hotmail.com>
2023-02-24 22:31:49 -0300

    identation

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom'
 const Home = (): any => {
   return (
     <div>
-        <h3>Cadastrar produto: <Link to={'product'}> Aqui </Link></h3>
-        <h3>Cadastrar loja: <Link to={'store'}> Aqui </Link></h3>
+        <h3>
+          Cadastrar produto: <Link to={'product'}> Aqui </Link>
+        </h3>
+        <h3>
+          Cadastrar loja: <Link to={'store'}> Aqui </Link>
+        </h3>
     </div>
   )
 }
