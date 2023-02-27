2662378176c8a7c651e07a0612e3ee46744f09ce
juliano <julianopaulo.santos@hotmail.com>
2023-02-27 19:36:40 -0300

    add form validator

--- src/Pages/Save/Product/index.tsx
+++ src/Pages/Save/Product/index.tsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { saveProduct } from '../../../firebase/collections/products'
-
 import { v4 as uuidv4 } from 'uuid'
 import { useNavigate } from 'react-router-dom'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+
+import { type ProductInterface, saveProduct } from '../../../firebase/collections/products'
 import { type StoreInterface, indexStores } from '../../../firebase/collections/stores'
 
 const Product = (): any => {
+  const { register, handleSubmit, formState: { errors } } = useForm<ProductInterface>()
+
   const [stores, setStores] = useState<StoreInterface[]>([])
-  const [storeId, setStoreId] = useState<string>('')
-  const [name, setName] = useState<string>('')
-  const [weight, setWeight] = useState<number>(0)
-  const [price, setPrice] = useState<number>(0)
-  const [stock, setStock] = useState<number>(0)
 
   const navigate = useNavigate()
 
@@ -23,28 +21,12 @@ const Product = (): any => {
     void fetchStores()
   }, [])
 
-  const save = async (): Promise<void> => {
-    const uuid = (document.getElementById('uuid') as HTMLInputElement).value
-    const data = {
-      storeId,
-      uuid,
-      name,
-      weight,
-      price,
-      stock
-    };
-
-    (document.getElementById('uuid') as HTMLInputElement).value = uuidv4();
-    (document.getElementById('store') as HTMLInputElement).value = ''
-    setStoreId('')
-    setName('')
-    setWeight(0)
-    setPrice(0)
-    setStock(0)
-
+  const save: SubmitHandler<ProductInterface> = async (data, event): Promise<void> => {
     await saveProduct(data)
       .then(() => { alert('produto salvo com sucesso') })
       .catch((e: string) => { alert(`erro ao salvar produto:\n${e}`) })
+
+    event?.target.reset()
   }
 
   return (
@@ -53,24 +35,31 @@ const Product = (): any => {
         voltar
       </button>
       <br/>
-      <input type="text" name='uuid' id="uuid" disabled defaultValue={uuidv4()} />
-      <br/>
-      <select name='store' id="store" onChange={e => { setStoreId(e.target.value) }}>
-        <option></option>
-        {stores.map((store, key) => (
-          <option key={key} value={store.uuid}>{store.name}</option>
-        ))}
-      </select>
-      <br/>
-      <input type="text" name='name' placeholder='name' required onChange={e => { setName(e.target.value) }} value={name} />
-      <br/>
-      <input type="number" name='weight' required onChange={e => { setWeight(Number(e.target.value)) }} value={weight} />
-      <br/>
-      <input type="number" name='price' required onChange={e => { setPrice(Number(e.target.value)) }} value={price} />
-      <br/>
-      <input type="number" name='stock' required onChange={e => { setStock(Number(e.target.value)) }} value={stock} />
-      <br/>
-      <button onClick={ () => { void save() } }>criar produto</button>
+      <form id="form" onSubmit={ (e) => { handleSubmit(save)(e).catch((error) => { console.log('error', error) }) } }>
+        <input type="text" name='uuid' id="uuid" disabled defaultValue={uuidv4()} />
+        <br/>
+        <select id="store" {...register('storeId', { required: true })}>
+          <option></option>
+          {stores.map((store, key) => (
+            <option key={key} value={store.uuid}>{store.name}</option>
+          ))}
+        </select>
+        {(Boolean(errors.storeId)) && <span>This field is required</span>}
+        <br/>
+        <input type="text" placeholder='name' {...register('name', { required: true })} />
+        {(Boolean(errors.name)) && <span>This field is required</span>}
+        <br/>
+        <input type="number" {...register('weight', { required: true, min: 1 })} />
+        {(Boolean(errors.weight)) && <span>This field is required</span>}
+        <br/>
+        <input type="number" {...register('price', { required: true, min: 1 })} />
+        {(Boolean(errors.price)) && <span>This field is required</span>}
+        <br/>
+        <input type="number" {...register('stock', { required: true, min: 0 })} />
+        {(Boolean(errors.stock)) && <span>This field is required</span>}
+        <br/>
+        <input type='submit' value='criar produto' />
+      </form>
     </div>
   )
 }
